b2c009b764ea5804727db4ee86dcd25bf45d2858
sschwindt <schwindt.water@gmail.com>
2020-11-19 11:46:16 +0100

    resolved merge conflict

--- get_h.py
+++ get_h.py
@@ -1,7 +1,7 @@
 import math as m
 
 
-def calc_discharge(b, h, k_st, m, S):
+def calc_discharge(b, h, k_st, m_bank, S):
     pass
 
 
@@ -13,9 +13,10 @@ if __name__ == '__main__':
     # input parameters
     Q = 15.5        # discharge in (m3/s)
     b = 5.1         # bottom channel width (m)
-    m = 2.5         # bank slope
-    n_m = 1/20      # Manning's n
+    m_bank = 2.5    # bank slope
+    k_st = 20       # Strickler value
+    n_m = 1 / k_st  # Manning's n
     S_0 = 0.005     # channel slope
 
     # call the solver with user-defined channel geometry and discharge
-    h_n = interpolate_h(Q, b, n_m=n_m, m=m, S0=S_0)
+    h_n = interpolate_h(Q, b, n_m=n_m, m_bank=m_bank, S0=S_0)
